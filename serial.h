b21a86c2642bc6b43191aa68655d0cdb71c87d02
Electronics Idiot <electronicsidiot@gmail.com>
2017-03-17 02:21:05 +1100

    Uncomment serial send in header

--- serial.h
+++ serial.h
@@ -1,5 +1,5 @@
-//#define SERIAL_SEND 1
-#define SERIAL_RECEIVE 5
+#define SERIAL_SEND 3
+#define SERIAL_RECEIVE 4
 #define SERIAL_RATE 9600
 
 #define SERIAL_BIT_TIME (1000000.0 / SERIAL_RATE) / (1.0 / 9.6 * 64)
